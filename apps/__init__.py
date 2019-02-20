63848135db65d2296e265079871b8728349f5e04
hosnicolina <corobonhosni@gmail.com>
2019-02-20 00:48:56 -0400

    se registro los blueprint core, posts, webs

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,5 +1,22 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['SECRET_KEY'] = 'mysecretkey'
 
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+from apps.core.views import core
+from apps.posts.views import posts
+
+app.register_blueprint(core)
+app.register_blueprint(posts)
