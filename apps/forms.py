629d582b139cfde2b9ddc976f17988397cf5abc1
hosnicolina <corobonhosni@gmail.com>
2019-02-20 00:51:17 -0400

    se movio los form a sus respectivos blueprint

--- apps/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, URL
-
-class UlrForm(FlaskForm):
-
-    url = StringField('Url de la web a revisar', validators=[DataRequired(), URL()])
-    submit = SubmitField('Revisar la web')
